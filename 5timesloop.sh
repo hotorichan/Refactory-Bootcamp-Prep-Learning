0ea60625b259afc6ab664f0e37338941102eaaa9
hotorichan <50395991+hotorichan@users.noreply.github.com>
2019-05-11 14:53:42 +0700

    Delete 5timesloop.sh

--- 5timesloop.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-echo "How many loop order do you want?" 
-read looporder
-loopcount=0
-while [ $loopcount -lt 5 ]
-do
-echo "Hello for $looporder times"
-loopcount=$(( $loopcount + 1)) 
-done
